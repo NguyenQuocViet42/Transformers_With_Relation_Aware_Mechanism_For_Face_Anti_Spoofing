10b8f8a7a1582474fe77999c664d695738503047
NguyenQuocViet42 <nqviet262@gmail.com>
2023-08-29 18:00:02 +0700

    add layer

--- model/Layer.py
+++ model/Layer.py
@@ -1,12 +1,14 @@
 import torch.nn as nn
 import torch
 from einops.layers.torch import Rearrange
+from transformers import ViTModel
+import torch.nn.functional as F
 
 
 class Input_layer(nn.Module):
     patch_size = 16
     num_patches = (224 // patch_size)**2 + 1
-    embed_dim = 196
+    embed_dim = 768
     
     def flatten_image(self, batch_image):
         output = []
@@ -36,10 +38,42 @@ class Input_layer(nn.Module):
         Zcls = self.cls_token.repeat(batch_image.size(0), 1, 1)
         Zcls_p = torch.cat((Zcls, Zp), dim=1)
         Zpos = self.positional_embedding
-        print(Zpos.shape)
         output = Zcls_p + Zpos
         return output
 
 class Encoder_layer(nn.Module):
     def __init__(self):
         super(Encoder_layer, self).__init__()
+        model_trans = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+        list_layer = list(model_trans.children())
+        self.encoder_layer = nn.Sequential(list_layer[1])
+        
+    def forward(self, x):
+        x = self.encoder_layer(x)
+        return x.last_hidden_state
+    
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(768, 4096)
+        self.norm_1 = nn.BatchNorm1d(4096)
+        self.gelu_1 = F.gelu()
+        self.dr_1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.norm_2 = nn.BatchNorm1d(4096)
+        self.gelu_2 = F.gelu()
+        self.dr_2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(4096, 10)
+
+        
+    def foward(self, x):
+        x = self.fc1(x)
+        x = self.norm_1(x)
+        x = self.gelu_1(x)
+        x = self.dr_1(x)
+        x = self.fc2(x)
+        x = self.norm_2(x)
+        x = self.gelu_2(x)
+        x = self.dr_2(x)
+        x = self.fc3(x)
+        return x
